3769db801816362bf1d4885de11a11be5faaf5bf
rrushman02 <r.rushman02@gmail.com>
2023-11-06 14:38:05 -0600

    Reworking newTask javascript

--- newTask.js
+++ newTask.js
@@ -1,3 +1,13 @@
 const taskJSONText = document.getElementById("taskJSONResult");
 taskJSONText.innerHTML = localStorage.getItem("JSONInfoTask");
 const formTask = document.querySelector("#taskForm");
+formTask.addEventListener("submit", function (e) {
+    let task = document.querySelector("#f-task").value;
+    if(task==null || task=="") {
+        alert("Enter a task to be assigned");
+        e.preventDefault();
+        return;
+    }
+
+    
+});
