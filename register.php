e5051c8ce71ca8ebf6cc3b04736402fc1331f576
rrushman02 <r.rushman02@gmail.com>
2023-12-07 16:47:24 -0600

    Mess around with form validation on newPerson again

--- register.php
+++ register.php
@@ -13,7 +13,87 @@ $user_err = $pass_err = $confirm_pass_err = "";
 //process data from form
 
 if($_SERVER["REQUEST_METHOD"] == "POST") {
-    if(empty(trim($_POST["user"])))
+    //validate username
+    if(empty(trim($_POST["user"]))) {
+        $user_err = "Please enter a username.";
+    }
+    elseif(!preg_match('/^[a-zA-Z0-9_]+$/', trim($_POST["user"]))) {
+        $user_err = "Username can only contain letters, numbers, and underscores.";
+    }
+    else {
+        //check is username already exists
+        $sql = "SELECT user FROM users WHERE user = ?";
+        if($stmt = mysqli_prepare($link, $sql)) {
+            //bind varaibles to prepared statement
+            mysqli_stmt_bind_param($stmt, "s", $paramuser);
+            $paramuser = trim($_POST["user"]);
+            //attempt to execute prepared statement
+            if(mysqli_stmt_execute($stmt)) {
+                if(mysqli_stmt_num_rows($stmt) != 0) {
+                    $user_err = "This username is already taken.";
+                }
+                else {
+                    $user = trim($_POST["user"]);
+                }
+            }
+            else {
+                echo "Something went wrong. Please try again later.";
+            }
+            mysqli_stmt_close($stmt);
+        }
+    }
+    //validate password
+    if(empty(trim($_POST["pass"]))) {
+        $pass_err = "Please enter a password.";
+    }
+    elseif(strlen(trim($_POST["pass"])) < 6) {
+        $pass_err = "Password must have at least 6 characters.";
+    }
+    else {
+        $pass = trim($_POST["pass"]);
+    }
+    //validate confirm password
+    if(empty(trim($_POST["confirm-pass"]))) {
+        $pass_err = "Please confirm password.";
+    }
+    else {
+        $confirm_pass = trim($_POST["confirm-pass"]);
+        if(empty($pass_err) && ($pass != $confirm_pass)) {
+            $confirm_pass_err = "Password did not match.";
+        }
+    }
+    //if firstname and lastname fields not empty, save those
+    if(!empty(trim($_POST["firstname"]))) {
+        $firstname = trim($_POST["firstname"]);
+    }
+    if(!empty(trim($_POST["lastname"]))) {
+        $lastname = trim($_POST["lastname"]);
+    }
+     //check input errors before inserting
+     if(empty($user_err) && empty($pass_err) && empty($confirm_pass_err)) {
+        //prepare insert statement
+        $sql = "INSERT INTO users (user, pass, firstname, lastname) VALUES 
+        (?, ?, ?, ?)";
+        if($stmt = mysqli_prepare($link, $sql)) {
+            mysqli_stmt_bind_param($stmt, "ssss", $paramuser, $parampass, $paramfname, $paramlname);
+            $paramuser = $user;
+            $parampass = password_hash($pass, PASSWORD_DEFAULT); //hash the password
+            $paramfname = $firstname;
+            $paramlname = $lastname;
+            //attempt to execute prepared statement
+            if(mysqli_stmt_execute($stmt)) {
+                //redirect to login page
+                header("location: login.php");
+            }
+            else {
+                echo "Something went wrong. Please try again later.";
+            }
+            //close statement
+            mysqli_stmt_close($stmt);
+        }
+     }
+     //close connection
+     mysqli_close($link);
 }
 ?>
 
@@ -50,7 +130,10 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
                     <?php echo (!empty($confirm_pass_err)) ? 'is-invalid' : ''; ?>"
                     value="<?php echo $confirm_pass; ?>">
                     <span class="invalid-feedback"><?php echo $confirm_pass_err; ?></span>
-
+                <label>First Name</label>
+                <input type="text" name=firstname>
+                <label>Last Name</label>
+                <input type=text name="lastname">
                 <input type="submit" value="Submit">
                 <input type="reset" value="Reset">
                 <p>Already have an account? <a href="login.php"><button>Login here</button>
