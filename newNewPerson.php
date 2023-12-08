ab1f2c07f7c32d90916aa19e727851a0bff490a3
rrushman02 <r.rushman02@gmail.com>
2023-12-08 10:22:49 -0600

    Add form elements

--- newNewPerson.php
+++ newNewPerson.php
@@ -17,12 +17,7 @@
                     <input type="text" name="firstname" placeholder="First" id="f-firstname" />
                     <span class="error">* <?php echo fnameErr;?></span>
                 </p>
-                <p>
-                    <label for="f-lastname">Last Name: </label>
-                    <input type="text" name="lastname" placeholder="Last" id="f-lastname" />
-                    <span class="error">* <?php echo lnameErr;?></span>
-                </p>
-                <input type="reset" />
+                
                 <input type="submit" />
             </fieldset>
         </form>
