abbe8af49dada9e402e0cf4ae43ed42daf031fe9
rrushman02 <r.rushman02@gmail.com>
2023-12-08 10:22:03 -0600

    Add form elements

--- newNewPerson.php
+++ newNewPerson.php
@@ -2,8 +2,33 @@
 
 <!DOCTYPE html>
 <html>
+<body>
     <h1>newPerson form plus form validation from PHP</h1>
     <p>Check what $_SERVER["PHP_SELF"] returns<p>
 
     <?php echo $_SERVER["PHP_SELF"];?>
+
+    <form method="POST"
+                action='<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>'>
+            <fieldset>
+                <legend>Add New Team Member</legend>
+                <p>
+                    <label for="f-firstname">First Name: </label>
+                    <input type="text" name="firstname" placeholder="First" id="f-firstname" />
+                    <span class="error">* <?php echo fnameErr;?></span>
+                </p>
+                <p>
+                    <label for="f-lastname">Last Name: </label>
+                    <input type="text" name="lastname" placeholder="Last" id="f-lastname" />
+                    <span class="error">* <?php echo lnameErr;?></span>
+                </p>
+                <input type="reset" />
+                <input type="submit" />
+            </fieldset>
+        </form>
+        <p id="personJSONResult">Team Member Info</p>
+
+        <script src="newPerson.js">
+        </script>
+</body>
 </html>
