879bca6681566c1b5221332211f182cbe57c66de
rrushman02 <r.rushman02@gmail.com>
2023-12-08 10:15:59 -0600

    Start debugging again

---
+++ newNewPerson.php
@@ -0,0 +1,9 @@
+
+
+<!DOCTYPE html>
+<html>
+    <h1>newPerson form plus form validation from PHP</h1>
+    <p>Check what $_SERVER["PHP_SELF"] returns<p>
+
+    <?php echo $_SERVER["PHP_SELF"];?>
+</html>
