1e954c97726b0a4793c46143f24e089a7f453da2
rrushman02 <r.rushman02@gmail.com>
2023-12-08 16:40:07 -0600

    Add styling:

--- newNewPerson.php
+++ newNewPerson.php
@@ -40,10 +40,44 @@
 
 <!DOCTYPE html>
 <html>
-<body>
     <h1>newPerson form plus form validation from PHP</h1>
 
-    <form id="personForm" method="POST"
+    <head>
+        <link rel="stylesheet" media='screen and (max-width: 480px)' href="mobile-style.css">
+        <link rel="stylesheet" media="screen and (min-width: 481px) and (max-width: 768px)" href="tablet-style.css">
+        <link rel="stylesheet" media="screen and (min-width: 769px)" href="desktop-style.css">
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=VT323&display=swap" rel="stylesheet">
+        <title>New Person</title>
+    </head>
+    <body>
+        <div class="container">
+            <p class="title">Adding a New Team Member</p>
+            <div class = "menu1">
+                <ul class = "menu_content"> 
+                    <li><a id="account_name" class="link" href="team.html"> Team Java </a></li> <!-- Task: Add links -->
+                    <li><a class= link href="frontpage.html">Front Page</a></li>
+                    <li><a class="link" href="person1.html"> Person 1 </a></li>
+                    <li><a id="sub_link"> Sub links </a></li>
+                    <li><a id="sub_link"> Sub links </a></li>
+                    <li><a id="sub_link"> Sub links </a></li>
+                    <li><a class="link" href="person2.html"> Person 2 </a></li>
+                    <li><a class="link" href="person3.html"> Person 3 </a></li>
+                    <li><a class="link" href="person4.html"> Person 4 </a></li>
+                    <li><a class="link" href="person5.html"> Person 5 </a></li>
+                    <li><a class="link" href="person6.html"> Person 6 </a></li>
+                    <li><a class="link" href="newPerson.html">Add New Team Member</a></li>
+                    <li><a class="link" href="newTask.html">Add New Task</a></li>
+                    <li><a class="link" href="progress_page.html"> Progress </a></li>
+                    <li><a id="settings" href="settings.html"> Settings </a></li>
+                </ul>
+            </div>
+        </div>
+
+        <form id="personForm" method="POST"
                 action='<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>'>
             <fieldset>
                 <legend>Add New Team Member</legend>
