5092fe0108521927a0ce9926640d41ba274f3578
rrushman02 <r.rushman02@gmail.com>
2023-12-09 17:40:51 -0600

    redo config file

--- config.php
+++ config.php
@@ -1,12 +1,19 @@
+//the login functionality is built off of the tutorial available at
+// https://www.tutorialrepublic.com/php-tutorial/php-mysql-login-system.php
+
 <?php
     define('DB_SERVER', localhost);
     define('DB_USERNAME', root);
     define('DB_PASSWORD', root);
     define('DB_NAME', 'users');
 
-    $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
-
-    if(!$link) {
-        die("ERROR: Could not connect. " . mysqli_connect_error());
+    try {
+        $pdo = new PDO("mysql:host=" . DB_SERVER . ";dbname=" . DN_NAME,
+                        DB_USERNAME, DB_PASSWORD);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO:ERRMODE_EXCEPTION;)
+        echo "Connected successfully";
+    }
+    catch (PDOException $e) {
+        die("ERROR: Could not connect." . $e->getMessage());
     }
 ?>
