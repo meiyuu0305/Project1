8d151312fda86d64839f0c4a44c6d0c4b24f0f6a
rrushman02 <r.rushman02@gmail.com>
2023-12-09 23:44:55 -0600

    comment out define lines

--- config.php
+++ config.php
@@ -2,15 +2,14 @@
 // https://www.tutorialrepublic.com/php-tutorial/php-mysql-login-system.php
 
 <?php
-    define('DB_SERVER', 'localhost');
+    /*define('DB_SERVER', 'localhost');
     define('DB_PORT', '3306')
     define('DB_USERNAME', 'root');
     define('DB_PASSWORD', 'root');
-    define('DB_NAME', 'time_management_proj');
+    define('DB_NAME', 'time_management_proj');*/
 
     try {
-        $pdo = new PDO("mysql:host=localhost;port=3306;dbname=time_management_proj",
-                        "root", "root");
+        $pdo = new PDO("mysql:host=localhost;port=3306;dbname=time_management_proj", "root", "root");
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO:ERRMODE_EXCEPTION;)
         echo "Connected successfully";
     }
