8c9a9c3f5f3773592a84141445e82ad905a3b61d
rrushman02 <r.rushman02@gmail.com>
2023-12-09 22:47:01 -0600

    Update port info in config.php

--- config.php
+++ config.php
@@ -3,12 +3,13 @@
 
 <?php
     define('DB_SERVER', 'localhost');
+    define('DB_PORT', '3306')
     define('DB_USERNAME', 'root');
     define('DB_PASSWORD', 'root');
     define('DB_NAME', 'time_management_proj');
 
     try {
-        $pdo = new PDO("mysql:host=" . DB_SERVER . ";dbname=" . DN_NAME,
+        $pdo = new PDO("mysql:host=" . DB_SERVER . ";port=" . DB_PORT . ";dbname=" . DN_NAME,
                         DB_USERNAME, DB_PASSWORD);
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO:ERRMODE_EXCEPTION;)
         echo "Connected successfully";
