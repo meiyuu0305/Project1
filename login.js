7391090696fd2852811d247f0ec61dff23ff497b
rrushman02 <r.rushman02@gmail.com>
2023-12-10 18:36:02 -0600

    Rewrite login.js file

--- login.js
+++ login.js
@@ -1,17 +1,16 @@
-personJSONText.innerHTML = localStorage.getItem("JSONLoginInfo");
 const formLogin = document.querySelector("#login_form");
 formLogin.addEventListener("submit", function (e) {
     //get and verify username field
     let username = document.querySelector("#username").value;
     if(username==null || username=="") {
-        alert("Enter first name");
+        alert("Enter username");
         e.preventDefault();
         return;
     }
     //get and verify password field
     let password = document.querySelector("#password").value;
     if(password==null || password=="") {
-        alert("Enter last name");
+        alert("Enter password");
         e.preventDefault();
         return;
     }
