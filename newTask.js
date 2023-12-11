c4819d9150440be81e1ad0744af703f93a263b12
rrushman02 <r.rushman02@gmail.com>
2023-12-11 14:32:37 -0600

    comment out unused code in javascript file

--- newTask.js
+++ newTask.js
@@ -1,24 +1,24 @@
 //set up which form to get field values from
-const taskJSONText = document.getElementById("taskJSONResult");
-taskJSONText.innerHTML = localStorage.getItem("JSONInfoTask");
+//const taskJSONText = document.getElementById("taskJSONResult");
+//taskJSONText.innerHTML = localStorage.getItem("JSONInfoTask");
 const formTask = document.querySelector("#taskForm");
 formTask.addEventListener("submit", function (e) {
     //get and verify task name
-    let ftask = document.querySelector("#f-task").value;
+    let ftask = document.querySelector("#task").value;
     if(ftask==null || ftask=="") {
         alert("Enter a task to be assigned");
         e.preventDefault();
         return;
     }
     //get and verify due date
-    let fdueDate = document.querySelector("#f-dueDate").value;
+    let fdueDate = document.querySelector("#dueDate").value;
     if(fdueDate==null || fdueDate=="") {
         alert("Choose a due date");
         e.preventDefault();
         return;
     }
     //get and verify team member field
-    let fteamMember = document.querySelector("#f-teamMember").value;
+    let fteamMember = document.querySelector("#teamMember").value;
     if(fteamMember==null || fteamMember=="") {
         alert("Choose a team member to assign the task to");
         e.preventDefault();
@@ -26,7 +26,7 @@ formTask.addEventListener("submit", function (e) {
     }
 
     //display field values in JSON on task page
-    const taskInfo = {Task:ftask, DueDate:fdueDate, TeamMember:fteamMember};
+    /*const taskInfo = {Task:ftask, DueDate:fdueDate, TeamMember:fteamMember};
     let taskTextJSON = JSON.stringify(taskInfo);
-    localStorage.setItem("JSONInfoTask", taskTextJSON);
+    localStorage.setItem("JSONInfoTask", taskTextJSON);*/
 });
