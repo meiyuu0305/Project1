0de8853cf7ca0c3437ff5a0620fbd8e651afa780
rrushman02 <r.rushman02@gmail.com>
2023-12-12 15:19:36 -0600

    comment out code to set up and display fake newPerson json

--- newPerson.js
+++ newPerson.js
@@ -1,6 +1,6 @@
 //set up which form to get items from
-const personJSONText = document.getElementById("personJSONResult");
-personJSONText.innerHTML = localStorage.getItem("JSONInfoPerson");
+//const personJSONText = document.getElementById("personJSONResult");
+//personJSONText.innerHTML = localStorage.getItem("JSONInfoPerson");
 const formPerson = document.querySelector("#personForm");
 formPerson.addEventListener("submit", function (e) {
     //get and verify first name field
@@ -18,7 +18,7 @@ formPerson.addEventListener("submit", function (e) {
         return;
     }
     //set up JSON object to be displayed on page
-    const teamMemberInfo = {FirstName:fname, LastName:lname};
-    let personTextJSON = JSON.stringify(teamMemberInfo);
-    localStorage.setItem("JSONInfoPerson", personTextJSON);
+    //const teamMemberInfo = {FirstName:fname, LastName:lname};
+    //let personTextJSON = JSON.stringify(teamMemberInfo);
+    //localStorage.setItem("JSONInfoPerson", personTextJSON);
 });
