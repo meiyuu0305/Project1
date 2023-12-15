6834bc342628e1a13b7cf4319604cd36ae84fc13
rrushman02 <r.rushman02@gmail.com>
2023-12-15 13:44:29 -0600

    remove database specification in connection and add if exists check for db creation

--- config.php
+++ config.php
@@ -7,14 +7,14 @@ https://www.tutorialrepublic.com/php-tutorial/php-mysql-login-system.php*/
 
 
     try {
-        $connString = "mysql:host=localhost;port=3306;dbname=time_management_proj";
+        $connString = "mysql:host=localhost;port=3306";
         $db_username = "root";
         $db_password = "root";
 
         $pdo = new PDO($connString, $db_username, $db_password);
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         //creating a database to store the logins
-        $sql = "CREATE DATABASE login_db";
+        $sql = "CREATE DATABASE IF NOT EXISTS login_db";
         $stmt = $pdo->prepare($sql);
         $stmt->execute();
         echo "Connection established and database created.";
