4084712a31bcbb3d1a44c6b113a541a8b769d9ee
rrushman02 <r.rushman02@gmail.com>
2023-12-15 13:58:56 -0600

    add table creation to config

--- config.php
+++ config.php
@@ -28,7 +28,8 @@ https://www.tutorialrepublic.com/php-tutorial/php-mysql-login-system.php*/
                         username VARCHAR(50) NOT NULL UNIQUE,
                         password VARCHAR(255) NOT NULL,
                         created_at DATETIME DEFAULT CURRENT_TIMESTAMP)";
-        $pdo->exec($sql);
+        
+        $pdo->exec($sqlTable);
         echo "Connection established and database created.";
 
     }
