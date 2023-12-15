cc186c5e7400fdbd71c16b9ccb987ad28ac26fdd
rrushman02 <r.rushman02@gmail.com>
2023-12-15 13:57:23 -0600

    add table creation to config

--- config.php
+++ config.php
@@ -14,13 +14,21 @@ https://www.tutorialrepublic.com/php-tutorial/php-mysql-login-system.php*/
         $pdo = new PDO($connString, $db_username, $db_password);
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         //creating a database to store the logins
-        $sql = "CREATE DATABASE IF NOT EXISTS login_db";
-        $stmt = $pdo->prepare($sql);
+        $sqlDb = "CREATE DATABASE IF NOT EXISTS login_db";
+        $stmt = $pdo->prepare($sqlDb);
         $stmt->execute();
         //reestablish connection for $pdo to newly created database
         $pdo = null;
         $connString = "mysql:host=localhost;port=3306;database=login_db";
         $pdo = new PDO($connString, $db_username, $db_password);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        //create table to store logins
+        $sqlTable = "CREATE TABLE IF NOT EXISTS logins (
+                        id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+                        username VARCHAR(50) NOT NULL UNIQUE,
+                        password VARCHAR(255) NOT NULL,
+                        created_at DATETIME DEFAULT CURRENT_TIMESTAMP)";
+        $pdo->exec($sql);
         echo "Connection established and database created.";
 
     }
