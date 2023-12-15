f0a6844787d4a3925dd933433ce9638b6a297f54
meiyuu0305 <mythaonguyen.work@gmail.com>
2023-12-15 11:18:08 -0600

    changes works on my machine

--- CreateDb.php
+++ CreateDb.php
@@ -14,7 +14,7 @@ class CreateDb
         $tablename = "Productdb",
         $servername = "localhost",
         $username = "root",
-        $password = "root"
+        $password = ""
     )
     {
       $this->dbname = $dbname;
