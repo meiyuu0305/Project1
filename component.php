f8f24e42c4b744de4e7b0714e9ae67cd6f054a5c
meiyuu0305 <121405997+meiyuu0305@users.noreply.github.com>
2023-12-15 17:45:03 -0600

    Update component.php

--- component.php
+++ component.php
@@ -35,12 +35,26 @@ function cartElement1($productid,$productname, $productprice, $productimg){
     ";
     echo $element;
 }
-function cartElement2($productid,$productname, $productprice, $productimg){
+function cartElement4($productid,$productname, $productprice, $productimg){
     $element = "
     <div class=\"checkout-items\">
-        <img src= \"$productimg\" style=\"width:100; height:100px; object-fit: cover;\"> 
-        <div class =\"products\" style=\"position: absolute; margin-left: 20px; height:40px; text-wrap:wrap; width: 150px\"> $productname </div>
+        <img src= \"$productimg\" style=\"width:250px; height:250px; object-fit: cover; margin-left: 70px;\"> 
+        <div class =\"products\" style=\"position: absolute; margin-left: 50px; height:40px; text-wrap:wrap; width: 150px\"> $productname </div>
         <div class =\"price\" style=\"font-size:50px;\"> $productprice</div>
+    </div>
+    ";
+    echo $element;
+}
+function cartElement2($order_id){
+    $element = "
+    <div class = \"checkout-head\" style=\"text-align: center;  background-color: black;color: white;\">Order number #$order_id</div>
+    ";
+    echo $element;
+}
+function cartElement3($orderPrice){
+    $element = "
+    <div class=\"hist-items\">
+        <div class =\"hist-product\"> $$orderPrice </div>
     </div>
     ";
     echo $element;
