8d4021fb32665426a78daaf3a80c990b09ce7c5d
rrushman02 <r.rushman02@gmail.com>
2023-12-15 13:41:00 -0600

    add command to create database to store logins

--- config.php
+++ config.php
@@ -13,6 +13,11 @@ https://www.tutorialrepublic.com/php-tutorial/php-mysql-login-system.php*/
 
         $pdo = new PDO($connString, $db_username, $db_password);
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        //creating a database to store the logins
+        $sql = "CREATE DATABASE login_db";
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute();
+        echo "Connection established and database created.";
     }
     catch (PDOException $e) {
         die("ERROR: Could not connect. " . $e->getMessage());
