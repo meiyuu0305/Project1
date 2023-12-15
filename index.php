2e50dc611b91cc90ac5ecbd3afedb384a94d86aa
meiyuu0305 <121405997+meiyuu0305@users.noreply.github.com>
2023-12-15 17:47:53 -0600

    Update index.php

--- index.php
+++ index.php
@@ -55,6 +55,7 @@ if (isset($_POST['add'])){
     <div id="highlight" style="background-color: none;"> team work makes the <span style="background-color: #a669f0; padding:0px 20px; "> dream</span> work</div>
     <img  style="position:absolute; right:150px" src="./upload/store-background.png" >
     <a href="frontpage.php" ><img src="./upload/home.png" style="margin: 30px 30px; width: 40px"> </a>
+    <a href="order_his.php"><button class ="shopping-bag" style="right:70px;"><img src="purchase_his.png" style="object-fit: cover; justify-content: center; height:100%; width:100%;"></button></a>
     <a href="cart.php" ><button class ="shopping-bag"><img src="./upload/shopping-bag-icon.png" style="object-fit: cover; justify-content: center; height:100%; width:100%"></button></a>
 </section>
 <section class = "store-scroll-menu">
